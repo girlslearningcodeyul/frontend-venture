adbe8ad52ae92b4e0bedc29b8cfbbad50eab8087
Jlahmy <jlahmy@yahoo.com>
2018-05-18 16:19:41 -0400

    sexy about

--- src/Intro.js
+++ src/Intro.js
@@ -72,7 +72,7 @@ class Content extends Component {
 
                 <Modal className="introModal" isOpen={this.state.modalOpen} toggle={this.toggleModal}>
                     <h2 align="center">Welcome traveller,</h2>
-                    <h2 align="center"> to the world of venture!</h2> 
+                    <h2 id="h2Modal" align="center"> to the world of venture!</h2> 
                     <h3 align="center">Crafted by a scrappy team of three aspiring master web-developers, venture is the brain-child of Aly Neuman, Ksenia Ndkn and Jordan Lahmy! </h3>
                     <h4 align="center"> Built with react/react-strap using express while written in javascript, what you see is the culmination of 10 days of intensive work!</h4>
                 </Modal>
