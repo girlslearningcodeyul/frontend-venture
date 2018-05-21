bed8ddf734d3fc79f0ff0679f4c4c640bbaa687e
girlslearningcodeyul <ksenia@ladieslearningcode.com>
2018-05-21 15:51:27 -0400

    catch null at Price

--- src/Price.js
+++ src/Price.js
@@ -29,6 +29,7 @@ class Content extends Component {
             isOpen: false,
             username: "",
             modalOpen: false,
+            isValid: true
         }
     }
 
@@ -47,27 +48,31 @@ class Content extends Component {
     handlePrice = (e) => {
         //add logic for the case where everything is set to false and null, in that case return a random generation of the listing
         e.preventDefault();
-        this.props.handleSubmit();
+        if (this.props.cheap || this.props.expensive) {
+            this.props.handleSubmit();
+        }
+        else {
+            this.setState({ isValid: false })
+        }
     }
 
     render() {
-        if(this.props.english === true) {
+        if (this.props.english === true) {
             return (
                 <div>
-    
                     <Modal className="introModal" isOpen={this.state.modalOpen} toggle={this.toggleModal}>
                         <h2 align="center">Welcome traveller,</h2>
                         <h2 id="h2Modal" align="center"> to the world of venture!</h2>
                         <h4 align="center">Créer avec amour par Aly Neumann, Ksenia Nadkina et Jordan Lahmy ! venture est née à base de React/react-strap utilisant express écrit en Javascript!  </h4>
                         <h4 align="center"> Ce que vous voyez est le produit de 10 jours intenses dans le but de redonner le sens de l'aventure en explorant votre ville!</h4>
                     </Modal>
-    
+
                     <Navbar color="light" light expand="md">
                         <NavbarBrand href="/">venture</NavbarBrand>
                         <NavbarToggler onClick={this.toggle} />
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className="ml-auto" navbar>
-                                <NavItem><NavLink onClick={this.props.toggleLanguage} style={{cursor:'pointer'}}>FR</NavLink></NavItem>
+                                <NavItem><NavLink onClick={this.props.toggleLanguage} style={{ cursor: 'pointer' }}>FR</NavLink></NavItem>
                                 <UncontrolledDropdown nav inNavbar>
                                     <DropdownToggle nav caret>Options</DropdownToggle>
                                     <DropdownMenu right>
@@ -79,47 +84,53 @@ class Content extends Component {
                             </Nav>
                         </Collapse>
                     </Navbar>
-    
+
                     <div className="priceContainer">
                         <Col sm="12" md={{ size: 8, offset: 2 }}>
                             <Label>{this.props.username}, choose your price range!</Label>
                         </Col>
-                        <Col sm="12" md={{ size: 8, offset: 2 }}>
-                            <Label check><CustomInput type="checkbox"
-                                value={this.props.cheap}
-                                onChange={() => this.props.togglePrice("cheap")} 
-                                id="k" label="$"/></Label>
-                        </Col>
-                        <Col sm="12" md={{ size: 8, offset: 2 }}>
-                            <Label check><CustomInput type="checkbox"
-                                value={this.props.expensive}
-                                onChange={() => this.props.togglePrice("expensive")} 
-                                id="m" label="$$"/></Label>
-                        </Col>
+                        <div>
+                            {!this.state.isValid && <div>Please, select at least one</div>}
+                            <Col sm="12" md={{ size: 8, offset: 2 }}>
+                                <Label className={!this.state.isValid && "formError"}>
+                                    <CustomInput type="checkbox"
+                                        value={this.props.cheap}
+                                        onChange={() => this.props.togglePrice("cheap")}
+                                        id="k" label="$" />
+                                </Label>
+                            </Col>
+                            <Col sm="12" md={{ size: 8, offset: 2 }}>
+                                <Label className={!this.state.isValid && "formError"}>
+                                    <CustomInput type="checkbox"
+                                        value={this.props.expensive}
+                                        onChange={() => this.props.togglePrice("expensive")}
+                                        id="m" label="$$" />
+                                </Label>
+                            </Col>
+                        </div>
                         <Col sm="12" md={{ size: 8, offset: 2 }}>
                             <Button onClick={this.handlePrice} >To greatness!</Button>
                         </Col>
-    
+
                     </div>
                 </div>
             );
         } else {
             return (
                 <div>
-    
                     <Modal className="introModal" isOpen={this.state.modalOpen} toggle={this.toggleModal}>
                         <h2 align="center">Welcome traveller,</h2>
                         <h2 id="h2Modal" align="center"> to the world of venture!</h2>
                         <h4 align="center">Crafted by a scrappy team of three aspiring web-developers, venture is the brain-child of Aly Neumann, Ksenia Nadkina and Jordan Lahmy! </h4>
                         <h4 align="center"> Built with react/react-strap using express & written in javascript, what you see is the culmination of 10 days of intensive work!</h4>
                     </Modal>
-    
+
                     <Navbar color="light" light expand="md">
                         <NavbarBrand href="/">venture</NavbarBrand>
                         <NavbarToggler onClick={this.toggle} />
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className="ml-auto" navbar>
-                                <NavItem><NavLink onClick={this.props.toggleLanguage} style={{cursor:'pointer'}}>EN</NavLink></NavItem>
+                                <NavItem><NavLink onClick={this.props.toggleLanguage} style={{ cursor: 'pointer' }}>EN</NavLink></NavItem>
                                 <UncontrolledDropdown nav inNavbar>
                                     <DropdownToggle nav caret>Options</DropdownToggle>
                                     <DropdownMenu right>
@@ -131,32 +142,39 @@ class Content extends Component {
                             </Nav>
                         </Collapse>
                     </Navbar>
-    
+
                     <div className="priceContainer">
                         <Col sm="12" md={{ size: 8, offset: 2 }}>
                             <Label>{this.props.username}, choisis ton prix</Label>
                         </Col>
-                        <Col sm="12" md={{ size: 8, offset: 2 }}>
-                            <Label check><CustomInput type="checkbox"
-                                value={this.props.cheap}
-                                onChange={() => this.props.togglePrice("cheap")} 
-                                id="k" label="$"/></Label>
-                        </Col>
-                        <Col sm="12" md={{ size: 8, offset: 2 }}>
-                            <Label check><CustomInput type="checkbox"
-                                value={this.props.expensive}
-                                onChange={() => this.props.togglePrice("expensive")} 
-                                id="m" label="$$"/></Label>
-                        </Col>
+                        <div>
+                            {!this.state.isValid && <div>Please, select at least one</div>}
+                            <Col sm="12" md={{ size: 8, offset: 2 }}>
+                                <Label className={!this.state.isValid && "formError"}>
+                                    <CustomInput type="checkbox"
+                                        value={this.props.cheap}
+                                        onChange={() => this.props.togglePrice("cheap")}
+                                        id="k" label="$" />
+                                </Label>
+                            </Col>
+                            <Col sm="12" md={{ size: 8, offset: 2 }}>
+                                <Label className={!this.state.isValid && "formError"}>
+                                    <CustomInput type="checkbox"
+                                        value={this.props.expensive}
+                                        onChange={() => this.props.togglePrice("expensive")}
+                                        id="m" label="$$" />
+                                </Label>
+                            </Col>
+                        </div>
                         <Col sm="12" md={{ size: 8, offset: 2 }}>
                             <Button onClick={this.handlePrice} >A la venture!</Button>
                         </Col>
-    
+
                     </div>
                 </div>
             );
         }
-        
+
     }
 }
 
