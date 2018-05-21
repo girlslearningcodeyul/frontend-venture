649cf8b83a37b78b4b8e29b1d9fc958a6f95c377
Jlahmy <jlahmy@yahoo.com>
2018-05-21 11:37:22 -0400

    choices page translations

--- src/Choices.js
+++ src/Choices.js
@@ -56,83 +56,165 @@ class Content extends Component {
 
     render() {
         //before load must generate a gif of typewriter typing out the experience
-        return (
-            <div>
-                <Modal className="introModal" isOpen={this.state.modalOpen} toggle={this.toggleModal}>
-                    <h2 align="center">Welcome traveller,</h2>
-                    <h2 id="h2Modal" align="center"> to the world of venture!</h2>
-                    <h4 align="center">Crafted by a scrappy team of three aspiring web-developers, venture is the brain-child of Aly Neumann, Ksenia Nadkina and Jordan Lahmy! </h4>
-                    <h4 align="center"> Built with react/react-strap using express & written in javascript, what you see is the culmination of 10 days of intensive work!</h4>
-                </Modal>
 
-                <Navbar color="light" light expand="md">
-                    <NavbarBrand className="brand" href="/">venture</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem><NavLink >FR</NavLink></NavItem>
-                            <UncontrolledDropdown nav inNavbar>
-                                <DropdownToggle nav caret>Options</DropdownToggle>
-                                <DropdownMenu right>
-                                    <DropdownItem><NavItem><NavLink href="/">Restart</NavLink></NavItem></DropdownItem>
-                                    <DropdownItem divider />
-                                    <DropdownItem onClick={this.toggleModal} >About</DropdownItem>
-                                </DropdownMenu>
-                            </UncontrolledDropdown>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-
-                <div className="choicesContainer">
-                    <Col sm="12" md={{ size: 8, offset: 2 }}>
-                        <Label>{this.props.username} A choose your own!</Label>
-                    </Col>
-                    <Row>
-                        <Col sm={{ size: 4, offset: 2 }}>
-                            <div>
-                                <li className="listingStyle">
-                                    <a href="tooltipLeft" id="tooltipLeft">
+        if(this.props.english === true) {
+            return (
+                <div>
+                    <Modal className="introModal" isOpen={this.state.modalOpen} toggle={this.toggleModal}>
+                        <h2 align="center">Welcome traveller,</h2>
+                        <h2 id="h2Modal" align="center"> to the world of venture!</h2>
+                        <h4 align="center">Crafted by a scrappy team of three aspiring web-developers, venture is the brain-child of Aly Neumann, Ksenia Nadkina and Jordan Lahmy! </h4>
+                        <h4 align="center"> Built with react/react-strap using express & written in javascript, what you see is the culmination of 10 days of intensive work!</h4>
+                    </Modal>
+    
+                    <Navbar color="light" light expand="md">
+                        <NavbarBrand className="brand" href="/">venture</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem><NavLink onClick={this.props.toggleLanguage}>FR</NavLink></NavItem>
+                                <UncontrolledDropdown nav inNavbar>
+                                    <DropdownToggle nav caret>Options</DropdownToggle>
+                                    <DropdownMenu right>
+                                        <DropdownItem><NavItem><NavLink href="/">Restart</NavLink></NavItem></DropdownItem>
+                                        <DropdownItem divider />
+                                        <DropdownItem onClick={this.toggleModal} >About</DropdownItem>
+                                    </DropdownMenu>
+                                </UncontrolledDropdown>
+                            </Nav>
+                        </Collapse>
+                    </Navbar>
+    
+                    <div className="choicesContainer">
+                        <Col sm="12" md={{ size: 8, offset: 2 }}>
+                            <Label>{this.props.username} A choose your own!</Label>
+                        </Col>
+                        <Row>
+                            <Col sm={{ size: 4, offset: 2 }}>
+                                <div>
+                                    <li className="listingStyle">
+                                        <a href="tooltipLeft" id="tooltipLeft">
+                                            <Link to={"/map?lat=" +
+                                                this.props.firstInterest.coordinates.lat + "&lng=" +
+                                                this.props.firstInterest.coordinates.long + "&step=" +
+                                                this.props.step}>
+                                                <img className="images" src={this.props.firstInterest.imageLocation} alt="img1" />
+                                            </Link>
+    
+                                        </a>
+                                        <br />
+                                        {this.props.firstInterest.name}
+                                    </li>
+                                    <Tooltip placement="left" isOpen={this.state.tooltipOpenLeft} target="tooltipLeft" toggle={this.toggleTooltipLeft}>
+                                        {this.props.firstInterest.description}
+                                    </Tooltip>
+                                </div>
+                            </Col>
+                            <Col sm={{ size: 4 }}>
+                                <div>
+                                    <li className="listingStyle">
+                                    <a href="tooltipRight" id="tooltipRight">
                                         <Link to={"/map?lat=" +
-                                            this.props.firstInterest.coordinates.lat + "&lng=" +
-                                            this.props.firstInterest.coordinates.long + "&step=" +
+                                            this.props.secondInterest.coordinates.lat + "&lng=" +
+                                            this.props.secondInterest.coordinates.long + "&step=" +
                                             this.props.step}>
-                                            <img className="images" src={this.props.firstInterest.imageLocation} alt="img1" />
-                                        </Link>
-
-                                    </a>
-                                    <br />
-                                    {this.props.firstInterest.name}
-                                </li>
-                                <Tooltip placement="left" isOpen={this.state.tooltipOpenLeft} target="tooltipLeft" toggle={this.toggleTooltipLeft}>
-                                    {this.props.firstInterest.description}
-                                </Tooltip>
-                            </div>
-                        </Col>
-                        <Col sm={{ size: 4 }}>
-                            <div>
-                                <li className="listingStyle">
-                                <a href="tooltipRight" id="tooltipRight">
-                                    <Link to={"/map?lat=" +
-                                        this.props.secondInterest.coordinates.lat + "&lng=" +
-                                        this.props.secondInterest.coordinates.long + "&step=" +
-                                        this.props.step}>
-                                        <img className="images" src={this.props.secondInterest.imageLocation} alt="img2" />
-                                    </Link> <br />
-                                    </a>
-                                    {this.props.secondInterest.name}
-                                </li >
-                                <Tooltip placement="right" isOpen={this.state.tooltipOpenRight} target="tooltipRight" toggle={this.toggleTooltipRight}>
-                                    {this.props.secondInterest.description}
-                                </Tooltip>
-
-                            </div>
-                        </Col>
-                        {/*use the toggle button method in bootstrap to reveal more text*/}
-                        <Col sm={{ size: 2 }} />
-                    </Row>
+                                            <img className="images" src={this.props.secondInterest.imageLocation} alt="img2" />
+                                        </Link> <br />
+                                        </a>
+                                        {this.props.secondInterest.name}
+                                    </li >
+                                    <Tooltip placement="right" isOpen={this.state.tooltipOpenRight} target="tooltipRight" toggle={this.toggleTooltipRight}>
+                                        {this.props.secondInterest.description}
+                                    </Tooltip>
+    
+                                </div>
+                            </Col>
+                            {/*use the toggle button method in bootstrap to reveal more text*/}
+                            <Col sm={{ size: 2 }} />
+                        </Row>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+            return (
+                <div>
+                    <Modal className="introModal" isOpen={this.state.modalOpen} toggle={this.toggleModal}>
+                        <h2 align="center">Welcome traveller,</h2>
+                        <h2 id="h2Modal" align="center"> to the world of venture!</h2>
+                        <h4 align="center">Crafted by a scrappy team of three aspiring web-developers, venture is the brain-child of Aly Neumann, Ksenia Nadkina and Jordan Lahmy! </h4>
+                        <h4 align="center"> Built with react/react-strap using express & written in javascript, what you see is the culmination of 10 days of intensive work!</h4>
+                    </Modal>
+    
+                    <Navbar color="light" light expand="md">
+                        <NavbarBrand className="brand" href="/">venture</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem><NavLink onClick={this.props.toggleLanguage}>EN</NavLink></NavItem>
+                                <UncontrolledDropdown nav inNavbar>
+                                    <DropdownToggle nav caret>Options</DropdownToggle>
+                                    <DropdownMenu right>
+                                        <DropdownItem><NavItem><NavLink href="/">Recommencer</NavLink></NavItem></DropdownItem>
+                                        <DropdownItem divider />
+                                        <DropdownItem onClick={this.toggleModal}>Détails</DropdownItem>
+                                    </DropdownMenu>
+                                </UncontrolledDropdown>
+                            </Nav>
+                        </Collapse>
+                    </Navbar>
+    
+                    <div className="choicesContainer">
+                        <Col sm="12" md={{ size: 8, offset: 2 }}>
+                            <Label>{this.props.username}, Choissiser le votre</Label>
+                        </Col>
+                        <Row>
+                            <Col sm={{ size: 4, offset: 2 }}>
+                                <div>
+                                    <li className="listingStyle">
+                                        <a href="tooltipLeft" id="tooltipLeft">
+                                            <Link to={"/map?lat=" +
+                                                this.props.firstInterest.coordinates.lat + "&lng=" +
+                                                this.props.firstInterest.coordinates.long + "&step=" +
+                                                this.props.step}>
+                                                <img className="images" src={this.props.firstInterest.imageLocation} alt="img1" />
+                                            </Link>
+    
+                                        </a>
+                                        <br />
+                                        {this.props.firstInterest.name}
+                                    </li>
+                                    <Tooltip placement="left" isOpen={this.state.tooltipOpenLeft} target="tooltipLeft" toggle={this.toggleTooltipLeft}>
+                                        {this.props.firstInterest.description}
+                                    </Tooltip>
+                                </div>
+                            </Col>
+                            <Col sm={{ size: 4 }}>
+                                <div>
+                                    <li className="listingStyle">
+                                    <a href="tooltipRight" id="tooltipRight">
+                                        <Link to={"/map?lat=" +
+                                            this.props.secondInterest.coordinates.lat + "&lng=" +
+                                            this.props.secondInterest.coordinates.long + "&step=" +
+                                            this.props.step}>
+                                            <img className="images" src={this.props.secondInterest.imageLocation} alt="img2" />
+                                        </Link> <br />
+                                        </a>
+                                        {this.props.secondInterest.name}
+                                    </li >
+                                    <Tooltip placement="right" isOpen={this.state.tooltipOpenRight} target="tooltipRight" toggle={this.toggleTooltipRight}>
+                                        {this.props.secondInterest.description}
+                                    </Tooltip>
+    
+                                </div>
+                            </Col>
+                            {/*use the toggle button method in bootstrap to reveal more text*/}
+                            <Col sm={{ size: 2 }} />
+                        </Row>
+                    </div>
+                </div>
+            );
+        }
+        
     }
 }
 
--- src/Preferences.js
+++ src/Preferences.js
@@ -272,7 +272,9 @@ class Preferences extends Component {
             historyPush={routeProps.history.push}
             step={routeProps.location.state}
             firstInterest={this.state.firstInterest}
-            secondInterest={this.state.secondInterest} />;
+            secondInterest={this.state.secondInterest}
+            toggleLanguage={this.props.toggleLanguage}
+            english = {this.props.english} />;
     }
 
     renderMap = (routeProps) => {
