f3150b1551f75672fad3c6375362b12ceb373145
Allison Neumann <allisonneumann@Allisons-MacBook-Pro.local>
2018-05-22 22:42:46 -0400

    cool fade in for rules

--- src/Rules.js
+++ src/Rules.js
@@ -78,9 +78,11 @@ class Content extends Component {
                         <Col sm={{ size: 8, offset: 2 }}>
                             <Label for="ventureWelcome2">
                                 <TypeWriter><h1>to the world of venture!</h1></TypeWriter>
-                                <h4>We will start by asking you questions in order to tailor your experience.</h4>
+                                <div className="rules-fade">
+                                    <h4>We will start by asking you questions in order to tailor your experience.</h4>
                                 <h4>You may answer more than once.</h4>
                                 <h4> So silence your phone, take a deep breath and throw yourselves to greatness!</h4>
+                                </div>
                             </Label>
                         </Col>
                         <Col sm="12" md={{ size: 8, offset: 2 }}>
@@ -123,10 +125,13 @@ class Content extends Component {
                         <Col sm={{ size: 8, offset: 2 }}>
                             <Label for="ventureWelcome2">
                                 <TypeWriter><h1>au monde de venture!</h1></TypeWriter>
+                                <div className="rules-fade">
                                 <h4>On va vous presenter des questions auquelles vous pouvez répondre plusieurs fois!</h4>
                                 <h4> À la fin, un choix de deux destinations.</h4>
                                 <h4>Bon voyage!</h4>
-                                <h4> Et à la venture!</h4></Label>
+                                <h4> Et à la venture!</h4>
+                                </div>
+                                </Label>
                         </Col>
                         <Col sm="12" md={{ size: 8, offset: 2 }}>
                             <Button onClick={this.handleRules} >Commencer</Button>
